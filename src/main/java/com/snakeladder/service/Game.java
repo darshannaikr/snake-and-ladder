3b38c6dab8e4167e9f4d055bbfce3528b991d0a7
Darshan Naik <rdarshannaik2503@gmail.com>
2026-05-08 13:30:46 +0530

    Added logging support using SLF4J and Logback

--- src/main/java/com/snakeladder/service/Game.java
+++ src/main/java/com/snakeladder/service/Game.java
@@ -2,6 +2,8 @@ package com.snakeladder.service;
 import com.snakeladder.board.Board;
 import com.snakeladder.model.Player;
 import com.snakeladder.util.Dice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
@@ -9,6 +11,7 @@ import java.util.*;
 
 public class Game {
 
+    private static final Logger logger = LoggerFactory.getLogger(Game.class);
     private Board board;
     private Queue<Player> players;
     private Dice dice;
@@ -37,14 +40,20 @@ public class Game {
 
             current.setPosition(newPosition);
 
-            System.out.println(
-                    current.getName() + " rolled a " + diceValue +
-                            " and moved from " + oldPosition +
-                            " to " + newPosition
-            );
+//            System.out.println(
+//                    current.getName() + " rolled a " + diceValue +
+//                            " and moved from " + oldPosition +
+//                            " to " + newPosition
+//            );
+            logger.info("{} rolled a {} and moved from {} to {}",
+                    current.getName(),
+                    diceValue,
+                    oldPosition,
+                    newPosition);
 
             if (newPosition == 100) {
-                System.out.println(current.getName() + " wins the game");
+//                System.out.println(current.getName() + " wins the game");
+                logger.info("{} wins the game", current.getName());
                 break;
             }
 
